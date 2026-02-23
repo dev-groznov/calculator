dd2a3d4d52c3e8cd056d0f2c48d3c14a1847140c
dev-groznov <devgroznov@gmail.com>
2026-02-23 10:27:58 +0300

    Create a new function operate
    it takes an operator and two numbers and then calls one of the above functions on the numbers.

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, b, sign) {
+    switch (sign) {
+        case '+': return add(a, b); break;
+        case '-': return subtract(a, b); break;
+        case '*': return multiply(a, b); break;
+        case '/': return divide(a, b); break;
+    }
+}
+
+let a, b, sign;
